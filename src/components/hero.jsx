750f2b293a688d914970a97401d6016a4d99d499
mgiftson0 <mgiftson00@gmail.com>
2025-01-09 22:30:26 +0000

    change

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -12,7 +12,7 @@ const Hero = () => {
           We help you achieve your goals with our amazing services.
         </p>
         <button className="px-6 py-3 bg-blue-500 text-white rounded hover:bg-blue-600">
-          Start 
+          Start ..
         </button>
       </section>
     </>
