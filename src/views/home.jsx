877d4de2e0cec7c7bfa082d44f151344c8a7c941
mgiftson0 <mgiftson00@gmail.com>
2025-01-13 15:46:32 +0000

    added the footer import

--- src/views/home.jsx
+++ src/views/home.jsx
@@ -6,7 +6,7 @@ import Card3 from "../components/Card3";
 import Card4 from "../components/Card4";
 import Testimonials from "../components/Testimonials";
 import Card5 from "../components/Card5";
-
+import Footer from "../components/Footer";
 
 
 const Home = () => {
@@ -30,6 +30,9 @@ const Home = () => {
       </div>
       <div className="mt-10"> 
         <Card5/>
+      </div>
+      <div className="mt-10"> 
+        <Footer/>
       </div>
       </div>
     </div>
