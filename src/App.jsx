6bbdb63f002b8db4fffddaf497260138189b5e7e
mgiftson0 <mgiftson00@gmail.com>
2025-01-14 17:21:13 +0000

    pricing page added

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./views/Home";
+// import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './views/Home';
+import Pricing from './views/Pricing';
+// Import other views as needed
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        {/* <Route path="/pricing" element={<div>Pricing Page</div>} /> */}
-        {/* <Route path="/about" element={<div>About Us Page</div>} /> */}
-        {/* <Route path="/contact" element={<div>Contact Page</div>} /> */}
-        {/* <Route path="/login" element={<div>Login Page</div>} /> */}
-        {/* <Route path="/signup" element={<div>Sign Up Page</div>} /> */}
+        <Route path="/pricing" element={<Pricing />} />
+        {/* Add other routes here */}
       </Routes>
     </Router>
   );
---
+++ src/views/Pricing.jsx
@@ -0,0 +1,14 @@
+import Header from '../components/Header';
+
+const Pricing = () => {
+  return (
+    <>
+      <Header />
+      <main className="mt-16">
+        {/* Content has been removed, leaving only the header */}
+      </main>
+    </>
+  );
+};
+
+export default Pricing;
