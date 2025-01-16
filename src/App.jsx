45ec0e2e4c17af1c24fdee41b8e616cb5cfca157
mgiftson0 <mgiftson00@gmail.com>
2025-01-16 19:52:30 +0000

    added the contact page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './views/Home';
 import Pricing from './views/Pricing';
 import AboutUs from './views/AboutUs'; 
+import Contact from './views/Contact';
 
 
 const App = () => {
@@ -11,6 +12,7 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/pricing" element={<Pricing />} />
         <Route path="/about-us" element={<AboutUs />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
     </Router>
   );
---
+++ src/views/Contact.jsx
@@ -0,0 +1,20 @@
+import Header from '../components/Header'; 
+import Footer from '../components/Footer';
+
+
+
+
+
+const AboutUs = () => {
+  return (
+    <div className="bg-gray-100">
+      <Header />
+    
+      <br/>
+      <br/>
+      <Footer /> 
+    </div>
+  ); 
+};
+
+export default AboutUs;
