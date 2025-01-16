21dea0332d9ad84130377688f942e68f965546c6
mgiftson0 <mgiftson00@gmail.com>
2025-01-16 19:46:03 +0000

    added card5 and footer

--- src/views/AboutUs.jsx
+++ src/views/AboutUs.jsx
@@ -2,6 +2,8 @@ import Header from '../components/Header';
 import About from '../components/About';
 import Mission from '../components/Mission';
 import Team from '../components/Team';
+import Card5 from '../components/Card5';
+import Footer from '../components/Footer';
 
 
 
@@ -14,6 +16,10 @@ const AboutUs = () => {
       <About />
       <Mission />
       <Team />
+      <Card5 />
+      <br/>
+      <br/>
+      <Footer />
     </div>
   ); 
 };
